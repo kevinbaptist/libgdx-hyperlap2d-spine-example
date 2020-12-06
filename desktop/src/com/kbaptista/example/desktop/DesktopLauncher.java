2d87a41f5c8fa22441ff6276436f24ea9e729123
Kevin <kbaptista258@gmail.com>
2020-12-06 18:35:56 +0000

    Init project

---
+++ desktop/src/com/kbaptista/example/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kbaptista.example.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kbaptista.example.Core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Core(), config);
+	}
+}
